0894d3e327fd2aebd04925576c8342841abe4dfe
cyyn <2908464947@qq.com>
2019-05-20 21:30:54 +0800

    first init

---
+++ src/main/java/com/haopeng/dakaqianndao/DakaqianndaoApplication.java
@@ -0,0 +1,13 @@
+package com.haopeng.dakaqianndao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DakaqianndaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DakaqianndaoApplication.class, args);
+    }
+
+}
