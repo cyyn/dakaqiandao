b6f8c9a8608559446daa1b6d37385109e34b6399
cyyn <2908464947@qq.com>
2019-05-27 20:26:48 +0800

    add component class

---
+++ src/main/java/com/haopeng/dakaqianndao/component/UserPosition.java
@@ -0,0 +1,10 @@
+package com.haopeng.dakaqianndao.component;
+
+import com.haopeng.dakaqianndao.vo.Position;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+
+@Component
+public class UserPosition extends HashMap<String, Position> {
+}
