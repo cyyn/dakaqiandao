0b411b2ba3a93041fdd1676ce538e2e32ee46269
cyyn <2908464947@qq.com>
2019-05-27 20:19:51 +0800

    add advsior class

---
+++ src/main/java/com/haopeng/dakaqianndao/advsior/ExceptionAdvisor.java
@@ -0,0 +1,28 @@
+package com.haopeng.dakaqianndao.advsior;
+
+import com.haopeng.dakaqianndao.dto.TextResMsg;
+import com.haopeng.dakaqianndao.exception.ClientException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+public class ExceptionAdvisor {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @ExceptionHandler
+    public ResponseEntity<TextResMsg> handleClientException(ClientException ex) {
+        logger.warn("ClientException, Errcode: {}, ErrMsg: {}", ex.getErrCode(), ex.getMessage());
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_XML);
+
+        TextResMsg textResMsg = new TextResMsg(ex.getOpenid(), ex.getMessage());
+        ResponseEntity<TextResMsg> textResMsgResponseEntity = new ResponseEntity<>(textResMsg, headers, HttpStatus.OK);
+        return textResMsgResponseEntity;
+    }
+}
